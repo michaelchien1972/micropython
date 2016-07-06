4a3203fd9ab41f653cd2d1e102c204e2b628e7c6
ChesterTseng <cwyark@gmail.com>
2016-07-07 02:11:05 +0800

    rtl8195a: fix connection error when AP mode and STA_AP mode
    add getnetif method in wlan object.

--- rlt8195a/mpconfigport.h
+++ rlt8195a/mpconfigport.h
@@ -50,6 +50,7 @@
 #define MICROPY_PY_UHASHLIB                     (1)
 #define MICROPY_PY_UCTYPES                      (1)
 #define MICROPY_PY_UJSON                        (1)
+#define MICROPY_PY_FRAMEBUF                     (1)
 #define MICROPY_PY_URE                          (1)
 #define MICROPY_PY_STRUCT                       (1)
 #define MICROPY_PY_SYS                          (1)
